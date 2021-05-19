e41c7b810a5f87ec4f7bacf0241ccb4f8c0d2a84
serjcoder <serj.coder@gmail.com>
2021-05-19 18:30:03 +0300

    fileManager in new branch

---
+++ fileManager/fileManager/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  fileManager
+//
+//  Created by Sergey Ivchenko on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+class FileStorage {
+
+    private lazy var data:[String:Any] = { () -> [String:Any] in
+        guard let dir = dir else { return [String : Any]()}
+        return NSDictionary(contentsOf: dir.appendingPathComponent(file)) as! [String: Any]
+    }()
+    static let shared = FileStorage()
+    private let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    private let file = "data.txt"
+    private func writeData(someData:Dictionary<String, Any>) {
+        let dictionary = someData as NSDictionary
+        guard let dir = dir else { return }
+        let fileURL = dir.appendingPathComponent(file)
+        dictionary.write(to: fileURL, atomically: false)
+    }
+    subscript (key: String) -> Any {
+        get {
+            data[key]
+        }
+        set {
+            data[key] = newValue
+            writeData(someData: data)
+        }
+    }
+}
