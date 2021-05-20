be23a3a73728b6e2f55335fddc0b7eeacafb04d6
serjcoder <serj.coder@gmail.com>
2021-05-20 20:13:48 +0300

    change let name

--- fileManager/fileManager/FileStorage.swift
+++ fileManager/fileManager/FileStorage.swift
@@ -21,17 +21,17 @@ class FileStorage {
         }
     }
     
-    private let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("data.txt")
+    private let fullPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("data.txt")
     
     private lazy var data:[String:Any] = { () -> [String:Any] in
-        guard let dir = dir else { return [String : Any]()}
-        guard let dict = NSDictionary(contentsOf: dir)  else { return [String:Any]() }
+        guard let fullPath = fullPath else { return [String : Any]()}
+        guard let dict = NSDictionary(contentsOf: fullPath)  else { return [String:Any]() }
         return dict as! [String: Any]
     }()
     
     private func writeData(someData:Dictionary<String, Any>) {
         let dictionary = someData as NSDictionary
-        guard let dir = dir else { return }
-        dictionary.write(to: dir, atomically: false)
+        guard let fullPath = fullPath else { return }
+        dictionary.write(to: fullPath, atomically: false)
     }
 }
