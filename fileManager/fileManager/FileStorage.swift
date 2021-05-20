cfcf954a2eafadd36056d1b8696c8538abb79f62
serjcoder <serj.coder@gmail.com>
2021-05-20 19:18:12 +0300

    fixing warnings and optional

--- fileManager/fileManager/FileStorage.swift
+++ fileManager/fileManager/FileStorage.swift
@@ -11,7 +11,7 @@ class FileStorage {
     
     static let shared = FileStorage()
     
-    subscript (key: String) -> Any {
+    subscript (key: String) -> Any? {
         get {
             data[key]
         }
@@ -21,20 +21,17 @@ class FileStorage {
         }
     }
     
-    private let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-    
-    private let file = "data.txt"
+    private let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("data.txt")
     
     private lazy var data:[String:Any] = { () -> [String:Any] in
         guard let dir = dir else { return [String : Any]()}
-        guard let dict = try? NSDictionary(contentsOf: dir.appendingPathComponent(file))  else { return [String:Any]() }
+        guard let dict = NSDictionary(contentsOf: dir)  else { return [String:Any]() }
         return dict as! [String: Any]
     }()
     
     private func writeData(someData:Dictionary<String, Any>) {
         let dictionary = someData as NSDictionary
         guard let dir = dir else { return }
-        let fileURL = dir.appendingPathComponent(file)
-        dictionary.write(to: fileURL, atomically: false)
+        dictionary.write(to: dir, atomically: false)
     }
 }
